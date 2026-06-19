c839d4772d24ff422728baa023d1689abf9375ec
Jake Atkins <atkins.jake@icloud.com>
2026-06-19 17:07:26 +0100

    removed the template data from the new react project, added a blank react flow canvas with grid based graph of height 600px with 100%, add put in empty arrays of nodes and edges as placeholder. Imported the canvas as the only element in the main App.jsx

---
+++ src/components/AutomataCanvas.jsx
@@ -0,0 +1,36 @@
+import {
+  ReactFlow,
+  Background,
+  BackgroundVariant,
+  Controls,
+} from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  {
+    id: "q0",
+    position: { x: 100, y: 100 },
+    data: { label: "q0" },
+  },
+];
+
+const initialEdges = [];
+
+function AutomataCanvas() {
+  return (
+    <div style={{ width: "100%", height: "600px" }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} fitView>
+        <Background
+          variant={BackgroundVariant.Lines}
+          gap={24}
+          size={1}
+        />
+
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+}
+
+export default AutomataCanvas;
